e7ae17b5a3210806873ba1fc46496e6f59d1c067
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-10 12:24:16 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -2,8 +2,8 @@
 name_of_person = "Jack the Ripper"
 puts "A group of flamingos is called #{name_of_person}"
 
-def greeting
-  puts "Hello, my little friend."
+def greeting(name_of_person)
+  puts "Hello, my little friend #{name_of_person}."
 end
 
 
